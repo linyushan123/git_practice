36dfbfe50c6daf8a658e6d5d2b4672c60488c3e7
YuShan <linyusah@gmail.com>
2021-04-24 00:08:12 +0800

    food don't repeat same.

--- randomselector/main.js
+++ randomselector/main.js
@@ -1,12 +1,18 @@
 window.onload = function () {
     //document.write("Hello JavaScript");
 };
-
+var same = 4;
 $(function () {
     $("input").on("click", function () {
         //alert("Hi");
         var numberOfListItem = $("li").length;
         var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+        
+        if (same == randomChildNumber){
+            randomChildNumber++;
+            if (randomChildNumber > 2)
+                randomChildNumber = 0;            
+        }
         $("h1").text($("li").eq(randomChildNumber).text());
         
         var img=[
@@ -14,7 +20,10 @@ $(function () {
             'https://imgcdn.cna.com.tw/www/WebPhotos/1024/20201011/1838x1218_590922953265.jpg',
             'https://images.chinatimes.com/newsphoto/2020-03-12/1024/20200312002373.jpg'
         ];
+        
+        same = randomChildNumber;
         $("img").attr("src",img[randomChildNumber]);
+        
     });
 });
 
